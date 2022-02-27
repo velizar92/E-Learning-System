f11a83ee2400d92feb0e334ba391aec42ea99c69
zako <velizar9209@gmail.com>
2022-02-27 19:51:31 +0200

    Implement some controller actions (All, Details and [POST]Delete)

--- E-LearningSystem/E-LearningSystem.Web/Controllers/CoursesController.cs
+++ E-LearningSystem/E-LearningSystem.Web/Controllers/CoursesController.cs
@@ -1,12 +1,51 @@
 ﻿namespace E_LearningSystem.Web.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-
+    using E_LearningSystem.Services.Services;
+    
     public class CoursesController : Controller
     {
+        private readonly ICourseService courseService;
+
+        public CoursesController(ICourseService _courseService)
+        {
+            this.courseService = _courseService;
+        }
+
+
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public async Task<IActionResult> All()
+        {
+            var courses = await courseService.GetAllCourses();
+
+            return View(courses);
+        }
+
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var courseDetails = await courseService.GetCourseDetails(id);
+
+            return View(courseDetails);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await courseService.DeleteCourse(id);
+
+            if(result == false)
+            {
+                return BadRequest();
+            }
+
+            return RedirectToAction("All");
         }
     }
 }
---
+++ E-LearningSystem/E-LearningSystem.Web/Models/Course/CourseDeleteViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace E_LearningSystem.Web.Models.Course
+{
+    public class CourseDeleteViewModel
+    {
+        public int Id { get; set; }
+    }
+}
